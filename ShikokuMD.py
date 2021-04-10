2fa12dd5a4376b44d5f0cffd8f7c8800708e9279
Shikoku <79487922+hoover362@users.noreply.github.com>
2021-04-10 09:43:56 -0500

    Update ShikokuMD.py

--- ShikokuMD.py
+++ ShikokuMD.py
@@ -61,18 +61,26 @@ def skad():
     except KeyboardInterrupt:
         sys.exit() 
 
-@client.event
-async def on_connect():
-  for user in client.user.friends:
-    try:
-      Shikoku = discord.Embed(color= discord.Color(000000))
-      Shikoku.set_footer(text="Shikoku")
-      Shikoku.set_author(name="")
-      await user.send(embed=Shikoku)
-      print(f"messaged: {user.name}")
-    except:
-       print(f"Message failed to send to: {user.name}")
-       print(f"Finished")
+    @client.event
+    async def on_connect():
+        friends = []
+        for i in massdm.client.user.friends:
+            friends.append(i)
+        massdm.banner()
+        input(f"{Fore.LIGHTGREEN_EX}Press any button to continue..")
+        messagetosend = input(f"{Fore.LIGHTGREEN_EX}Insert message to send: ")
+        print("Starting...")
+        utils.rename(f"Sending message to {len(friends)} friends..")
+        for i in friends:
+            try:
+                await i.send(messagetosend)
+                print(f"{Fore.LIGHTCYAN_EX}Message sent to: {i.name}{Fore.RESET}")
+            except Exception as err:
+                print(f"{Fore.RED} Error sending DM to {i.name}: {err}{Fore.RESET}")
+        input(f"{Fore.GREEN}Message have been dmed to {len(friends)} friends! Press any button to exit..")
+        utils.rename("Done sending messages")
 
+    def run(token):
+        massdm.client.run(token, bot=False)
 
-client.run(token, bot=False)
+massdm.run(token)
